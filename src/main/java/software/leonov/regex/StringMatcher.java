31f143873000c55c90e6d41d0ab0500532284532
zhenya <leonov@gmail.com>
2020-03-19 11:36:29 -0700

    Miscellaneous refactoring

--- src/main/java/software/leonov/regex/StringMatcher.java
+++ src/main/java/software/leonov/regex/StringMatcher.java
@@ -5,6 +5,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.time.Duration;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Supplier;
 import java.util.regex.MatchResult;
 
 /**
@@ -53,166 +54,69 @@ public abstract class StringMatcher<T> implements MatchResult {
      * 
      * @return {@code true} if, and only if, the entire input string matches
      */
-    public abstract boolean matches();
-
-    /**
-     * Spends at most {@code duration} time attempting to match the entire string against the regular-expression. If the
-     * specified duration is exceeded this method will throw a {@code TimeoutException}.
-     * 
-     * @param duration the time to wait for the operation to complete before abandoning it, a value of {@code 0} indicates
-     *                 to wait forever and is equivalent to calling {@link #matches()}
-     * @return {@code true} if, and only if, the entire input string matches
-     * @throws InterruptedException if current thread is interrupted (the interrupted status of the current thread is
-     *                              cleared when this exception is thrown)
-     * @throws TimeoutException     if the time out has been exceeded while attempting to find the next match
-     * @throws Throwable            if any other errors occurs
-     */
-    @SuppressWarnings("deprecation")
-    public boolean matches(final Duration duration) throws Throwable {
-        checkNotNull(duration, "duration == null");
-        checkArgument(!duration.isNegative(), "duration < 0");
-
-        if (duration.isZero())
-            return matches();
-
-        final long millis = duration.toMillis();
-
-        final Thread t = new Thread(matcher);
-
-        InterruptedException ie = null;
-
-        t.start();
-        try {
-            t.join(millis);
-        } catch (final InterruptedException e) {
-            ie = e;
-        }
-
-        if (t.isAlive()) {
-            /*
-             * Even though Thread.stop() is deprecated, in this case there are no objects used by Thread "t" that are referenced
-             * anywhere else, thus we don't care if they are left in an inconsistent or otherwise damaged state, they will just be
-             * garbage collected. Likewise Thread "t" is not holding any resources that need to be closed or otherwise released.
-             */
-            t.stop();
-            match = false;
-            if (ie == null)
-                throw new TimeoutException("match operation exceeded " + duration);
-            else
-                throw ie;
-        } else {
-            if (matcher.getThrowable() == null)
-                return matcher.getValue();
-            else
-                throw matcher.getThrowable();
-        }
-
-    }
-
-    private Matcher matcher = new Matcher();
-
-    private class Matcher implements Runnable {
-        boolean result = false;
-        Throwable t = null;
-
-        @Override
-        public void run() {
-            try {
-                result = matches();
-            } catch (final Throwable throwable) {
-                t = throwable;
-            }
-        }
-
-        public boolean getValue() {
-            return result;
-        }
-
-        public Throwable getThrowable() {
-            return t;
-        }
-
-    }
+    protected abstract boolean findImpl();
 
     /**
      * Attempts to find the next substring of the input string that matches the regular-expression.
      * 
      * @return {@code true} if, and only if, a substring of the input string is a match
      */
-    public abstract boolean find();
+    protected abstract boolean matchesImpl();
+
+    /**
+     * Attempts to match the entire string against the regular-expression.
+     * 
+     * @return {@code true} if, and only if, the entire input string matches
+     * @throws InterruptedException if current thread is interrupted (the interrupted status of the current thread is
+     *                              cleared when this exception is thrown)
+     * @throws Throwable            if any other errors occurs
+     */
+    public boolean matches() throws Throwable {
+        return matches(Duration.ZERO);
+    }
+
+    /**
+     * Spends at most {@code duration} time attempting to match the entire string against the regular-expression. If the
+     * specified duration is exceeded this method will throw a {@code TimeoutException}.
+     * 
+     * @param duration the time to wait for the operation to complete before abandoning it, a value of {@code 0} indicates
+     *                 to wait forever
+     * @return {@code true} if, and only if, the entire input string matches
+     * @throws InterruptedException if current thread is interrupted (the interrupted status of the current thread is
+     *                              cleared when this exception is thrown)
+     * @throws TimeoutException     if the time out has been exceeded while attempting to find the next match
+     * @throws Throwable            if any other errors occurs
+     */
+    public boolean matches(final Duration duration) throws Throwable {
+        return resultOf(matches, duration);
+    }
+
+    /**
+     * Attempts to find the next substring of the input string that matches the regular-expression.
+     * 
+     * @return {@code true} if, and only if, a substring of the input string is a match
+     * @throws InterruptedException if current thread is interrupted (the interrupted status of the current thread is
+     *                              cleared when this exception is thrown)
+     * @throws Throwable            if any other errors occurs
+     */
+    public boolean find() throws Throwable {
+        return find(Duration.ZERO);
+    }
 
     /**
      * Spends at most {@code duration} time attempting to find the next substring of the input string that matches the
      * regular-expression. If the specified duration is exceeded this method will throw a {@code TimeoutException}.
      * 
      * @param duration the time to wait for the operation to complete before abandoning it, a value of {@code 0} indicates
-     *                 to wait forever and is equivalent to calling {@link #find()}
+     *                 to wait forever
      * @return {@code true} if, and only if, a substring of the input string is a match
      * @throws TimeoutException     if the time out has been exceeded while attempting to find the next match
      * @throws InterruptedException if current thread is interrupted (the interrupted status of the current thread is
      *                              cleared when this exception is thrown)
      * @throws Throwable            if any other errors occurs
      */
-    @SuppressWarnings("deprecation")
     public boolean find(final Duration duration) throws Throwable {
-        checkNotNull(duration, "duration == null");
-        checkArgument(!duration.isNegative(), "duration < 0");
-
-        if (duration.isZero())
-            return find();
-
-        final long millis = duration.toMillis();
-
-        final Thread t = new Thread(finder);
-
-        InterruptedException ie = null;
-
-        t.start();
-        try {
-            t.join(millis);
-        } catch (final InterruptedException e) {
-            ie = e;
-        }
-
-        if (t.isAlive()) {
-            /*
-             * Even though Thread.stop() is deprecated, in this case there are no objects used by Thread "t" that are referenced
-             * anywhere else, thus we don't care if they are left in an inconsistent or otherwise damaged state, they will just be
-             * garbage collected. Likewise Thread "t" is not holding any resources that need to be closed or otherwise released.
-             */
-            t.stop();
-            // silently swallows any exceptions which occur
-            match = false;
-            if (ie == null)
-                throw new TimeoutException("match operation exceeded " + duration);
-            else
-                throw ie;
-        } else
-            return finder.get();
-
-    }
-
-    private Finder finder = new Finder();
-
-    private class Finder implements Runnable {
-        boolean result = false;
-        Throwable t = null;
-
-        @Override
-        public void run() {
-            try {
-                result = find();
-            } catch (final Throwable throwable) {
-                t = throwable;
-            }
-        }
-
-        public boolean get() throws Throwable {
-            if (t != null)
-                throw t;
-            return result;
-        }
-
+        return resultOf(find, duration);
     }
 
     /**
@@ -249,7 +153,7 @@ public abstract class StringMatcher<T> implements MatchResult {
      * <p>
      * This method is intended to be used in a loop together with the {@link #appendTail(StringBuilder)} and {@link #find()}
      * methods. For example, the following code writes {@code one dog two dogs in the
-     * yard} to the standard out: <blockquote>
+     * yard} to the standard out:
      * 
      * <pre>
      * final RegularExpression r = JDKRegularExpression.compile(&quot;cat&quot;);
@@ -261,8 +165,6 @@ public abstract class StringMatcher<T> implements MatchResult {
      * m.appendTail(sb);
      * System.out.println(sb);
      * </pre>
-     * 
-     * </blockquote>
      * 
      * This method is modeled after {@link java.util.regex.Matcher#appendReplacement(StringBuffer, String)
      * Matcher.appendReplacement(StringBuffer, String)}.
@@ -280,36 +182,33 @@ public abstract class StringMatcher<T> implements MatchResult {
         lastAppendPosition = end();
 
         int index = 0;
-        final StringBuffer result = new StringBuffer();
+        final StringBuilder result = new StringBuilder();
 
         while (index < replacement.length()) {
             char ch = replacement.charAt(index);
             if (ch == '$') {
                 index++;
-                int num = Character.getNumericValue(replacement.charAt(index++));
-                checkArgument(num >= 0 && num <= 9, "illegal group reference");
-                int reference = num;
+                int groupId = getIntValue(replacement.charAt(index++));
+                checkArgument(groupId >= 0 && groupId <= 9, "illegal group reference");
 
-                for (;;) {
-                    if (index < replacement.length())
+                while (index < replacement.length()) {
+                    int n = getIntValue(replacement.charAt(index));
+
+                    if (n < 0 || n > 9)
                         break;
 
-                    num = Character.getNumericValue(replacement.charAt(index));
-
-                    if (num < 0 || num > 9)
-                        break;
-
-                    int count = reference * 10 + num;
+                    int count = groupId * 10 + n;
 
                     if (count < groupCount()) {
-                        reference = count;
+                        groupId = count;
                         index++;
                     } else
                         break;
                 }
 
-                if (group(reference) != null)
-                    result.append(group(reference));
+                if (group(groupId) != null)
+                    result.append(group(groupId));
+
             } else if (ch == '\\') {
                 index++;
                 result.append(replacement.charAt(index++));
@@ -350,8 +249,11 @@ public abstract class StringMatcher<T> implements MatchResult {
      * 
      * @param replacement the replacement string
      * @return the string constructed by replacing each match with the replacement string
+     * @throws InterruptedException if current thread is interrupted (the interrupted status of the current thread is
+     *                              cleared when this exception is thrown)
+     * @throws Throwable            if any other errors occurs
      */
-    public String replaceAll(final String replacement) {
+    public String replaceAll(final String replacement) throws Throwable {
         checkNotNull(replacement, "replacement == null");
         reset();
         final StringBuilder sb = new StringBuilder();
@@ -373,11 +275,12 @@ public abstract class StringMatcher<T> implements MatchResult {
      * 
      * @param replacement the replacement string
      * @param duration    the time to wait for the operation to complete before abandoning it, a value of {@code 0}
-     *                    indicates to wait forever and is equivalent to calling {@link #replaceAll(String)}
+     *                    indicates to wait forever
+     * @return the string constructed by replacing each match with the replacement string
      * @throws TimeoutException     if the time out has been exceeded while attempting to find the next match
      * @throws InterruptedException if current thread is interrupted (the interrupted status of the current thread is
      *                              cleared when this exception is thrown)
-     * @return the string constructed by replacing each match with the replacement string
+     * @throws Throwable            if any other errors occurs
      */
     public String replaceAll(final String replacement, final Duration duration) throws Throwable {
         checkNotNull(replacement, "replacement == null");
@@ -400,8 +303,11 @@ public abstract class StringMatcher<T> implements MatchResult {
      * 
      * @param replacement the replacement string
      * @return the string constructed by replacing the first match with the replacement string
+     * @throws InterruptedException if current thread is interrupted (the interrupted status of the current thread is
+     *                              cleared when this exception is thrown)
+     * @throws Throwable            if any other errors occurs
      */
-    public String replaceFirst(final String replacement) {
+    public String replaceFirst(final String replacement) throws Throwable {
         checkNotNull(replacement, "replacement == null");
         final StringBuilder sb = new StringBuilder();
         reset();
@@ -423,11 +329,12 @@ public abstract class StringMatcher<T> implements MatchResult {
      * 
      * @param replacement the replacement string
      * @param duration    the time to wait for the operation to complete before abandoning it, a value of {@code 0}
-     *                    indicates to wait forever and is equivalent to calling {@link #replaceFirst(String)}
+     *                    indicates to wait forever
+     * @return the string constructed by replacing the first match with the replacement string
      * @throws TimeoutException     if the time out has been exceeded while attempting to find the next match
      * @throws InterruptedException if current thread is interrupted (the interrupted status of the current thread is
      *                              cleared when this exception is thrown)
-     * @return the string constructed by replacing the first match with the replacement string
+     * @throws Throwable            if any other errors occurs
      */
     public String replaceFirst(final String replacement, final Duration duration) throws Throwable {
         checkNotNull(replacement, "replacement == null");
@@ -437,6 +344,63 @@ public abstract class StringMatcher<T> implements MatchResult {
             appendReplacement(sb, replacement);
         appendTail(sb);
         return sb.toString();
+    }
+
+    private final MatchOperation matches = new MatchOperation(() -> matchesImpl());
+    private final MatchOperation find = new MatchOperation(() -> findImpl());
+
+    private final class MatchOperation implements Runnable {
+
+        private final Supplier<Boolean> supplier;
+        private boolean result = false;
+        private Throwable t = null;
+
+        public MatchOperation(final Supplier<Boolean> supplier) {
+            this.supplier = supplier;
+        }
+
+        @Override
+        public void run() {
+            try {
+                result = supplier.get();
+            } catch (final Throwable x) {
+                t = x;
+            }
+        }
+
+        public boolean result() throws Throwable {
+            if (t != null)
+                throw t;
+            return result;
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private boolean resultOf(final MatchOperation operation, final Duration duration) throws Throwable {
+        checkNotNull(duration, "duration == null");
+        checkArgument(!duration.isNegative(), "duration < 0");
+
+        InterruptedException x = null;
+
+        final Thread t = new Thread(operation);
+        t.start();
+
+        try {
+            t.join(duration.toMillis());
+        } catch (final InterruptedException e) {
+            x = e;
+        }
+
+        if (t.isAlive()) {
+            t.stop();
+            match = false;
+            throw x != null ? x : new TimeoutException("match operation exceeded " + duration);
+        } else
+            return operation.result();
+    }
+
+    private static int getIntValue(final int c) {
+        return c - '0';
     }
 
 }
